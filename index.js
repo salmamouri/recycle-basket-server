ad51a39be2fc975142a1ff1f7e8898e374f23cbb
Salma Tabassum Mouri <mouriclub@gmail.com>
2022-11-25 22:53:43 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 const app = express();
 
@@ -13,3 +14,15 @@ app.get('/',async(req,res)=>{
 });
 
 app.listen(port,()=> console.log(`Recycle Basket is running on ${port}`))
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iykcv1w.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
